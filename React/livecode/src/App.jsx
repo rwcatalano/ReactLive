5599fd8f500cfe6e085b64e01aed7a5e92a0efaf
Rob Catalano <rob@adventurebot.com>
2022-04-20 08:27:42 -0500

    Display Response

--- React/livecode/src/App.jsx
+++ React/livecode/src/App.jsx
@@ -7,19 +7,31 @@ import axios from 'axios';
 function App() {
 
   const [message, setMessage] = useState('Welcome');
+  const [data, setData] = useState();
 
-  const btnClick = () => {
+
+  const btnClick = (e) => {
     console.log(message)
-
+    var currency = e.currentTarget.getAttribute('data-id')
+    
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then((response) => {
+      let d = response.data.bpi;
+      switch(currency)
+      {
+        case 'usd':
+          setData(d.USD);
+          break;
+          case 'gbp':
+          setData(d.GBP);
+          break;
+          case 'eur':
+          setData(d.EUR);
+          break;
+      }
+
       console.log(response)
     })
 
-
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then((response) => {
-      console.log(response);
-      console.log(response.data.bpi.USD);
-  })
 
   }
 
@@ -41,11 +53,25 @@ function App() {
           Learn React
         </a>
 
+<div class="row">
 <div class="col-4 offset-col-4">
   <input type="text" value={message} onChange={(e) => setMessage(e.currentTarget.value)} class="form-control"/>
-  <button type="button" onClick={btnClick} class="btn btn-info">Go Go</button>
+  <button type="button" onClick={btnClick} class="btn btn-info" data-id="usd">Usd</button>
+  <button type="button" onClick={btnClick} class="btn btn-info" data-id="eur">Eur</button>
+  <button type="button" onClick={btnClick} class="btn btn-info" data-id="gbp">Gbp</button>
+</div>
 </div>
 
+<div class="row">
+<div class="col-4 offset-col-4">
+  <table>
+    <tbody>
+      <tr><td>{data?.description}</td></tr>
+      <tr><td>{data?.rate}</td></tr>
+    </tbody>
+  </table>
+</div>
+</div>
       </header>
 
 
